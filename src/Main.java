e28f2647f75df672e9e71d59e145802d3143d868
Katya <dolgova01011987@gmail.com>
2024-02-27 18:23:25 +0300

    Initiall commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(1.87,98);
+        double bmi = service.calculate(1.87, 98);
 
 
         System.out.println("BMI: " + bmi);
